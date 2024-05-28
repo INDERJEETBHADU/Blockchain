5a5247d44ff6f67ab518a5ddd6e51d23368a162c
INDERJEETBHADU <bishnoiinderjeet580@gmail.com>
2024-05-28 09:29:34 +0530

    link_upadted

--- src/common/Allsocial.jsx
+++ src/common/Allsocial.jsx
@@ -8,11 +8,21 @@ import graph from "../assets/images/svg/graph.svg";
 function Allsocial() {
   return (
     <div className=" d-flex flex-wrap align-items-center gap-sm-4 gap-lg-1 gap-xl-4 gap-3">
-      <img src={detaabout} alt="deta" />
-      <img src={horse} alt="deta" />
-      <img src={twiiter} alt="deta" />
-      <img src={Telegramm} alt="deta" />
-      <img src={graph} alt="deta" />
+      <a href="https://www.dextools.io/app/en/pairs" target="_blank">
+        <img src={detaabout} alt="deta" />
+      </a>
+      <a href="">
+        <img src={horse} alt="deta" />
+      </a>
+      <a href="https://x.com/" target="_blank">
+        <img src={twiiter} alt="deta" />
+      </a>
+      <a href="https://www.telegram.com/" target="_blank">
+        <img src={Telegramm} alt="deta" />
+      </a>
+      <a href="">
+        <img src={graph} alt="deta" />
+      </a>
     </div>
   );
 }
