e4ed2ca64f8ef4acf02ce91c07acc89031374c6a
INDERJEETBHADU <bishnoiinderjeet580@gmail.com>
2024-05-28 14:34:02 +0530

    ellipse_updated

--- src/common/Allsocial.jsx
+++ src/common/Allsocial.jsx
@@ -7,20 +7,28 @@ import graph from "../assets/images/svg/graph.svg";
 
 function Allsocial() {
   return (
-    <div className=" d-flex flex-wrap align-items-center gap-sm-4 gap-lg-1 gap-xl-4 gap-3">
-      <a href="https://www.dextools.io/app/en/pairs" target="_blank">
+    <div className=" d-flex  align-items-center gap-sm-4 gap-lg-1 gap-xl-4 gap-1">
+      <a
+        className=" icon_width w-100"
+        href="https://www.dextools.io/app/en/pairs"
+        target="_blank"
+      >
         <img src={detaabout} alt="deta" />
       </a>
-      <a href="">
+      <a className=" icon_width w-100" href="">
         <img src={horse} alt="deta" />
       </a>
-      <a href="https://x.com/" target="_blank">
+      <a className=" icon_width w-100" href="https://x.com/" target="_blank">
         <img src={twiiter} alt="deta" />
       </a>
-      <a href="https://www.telegram.com/" target="_blank">
+      <a
+        className=" icon_width w-100"
+        href="https://www.telegram.com/"
+        target="_blank"
+      >
         <img src={Telegramm} alt="deta" />
       </a>
-      <a href="">
+      <a className=" icon_width w-100" href="">
         <img src={graph} alt="deta" />
       </a>
     </div>
